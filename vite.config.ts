5d0b19e6d1e26b01fcb0b1b2cfa9fbf2f125c62f
TOMIMOE1226 <tomimoe@outlook.jp>
2025-06-30 02:41:56 +0900

    fix: add base path for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,12 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  // ←★ 追加した行
+  base: "/taskun/",          // GitHub Pages 上での「貼り付け場所」の指定
   plugins: [react()],
   optimizeDeps: {
-    exclude: ['lucide-react'],
+    exclude: ["lucide-react"],
   },
 });
